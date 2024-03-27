1fc609a0a7a6ee247502c4c67f9a79cfda06c786
Patrick Krueger <sendthistopatrick@gmail.com>
2024-03-26 21:08:30 -0600

    replace mini status line with lualine

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -5,6 +5,19 @@ return {
   config = function()
     require('catppuccin').setup {
       flavour = 'macchiato',
+      integrations = {
+        gitsigns = true,
+        mason = true,
+        mini = {
+          enabled = true,
+          indentscope_color = '',
+        },
+        neotree = true,
+        treesitter = true,
+        telescope = {
+          enabled = true,
+        },
+      },
       -- transparent_background = false,
       -- color_overrides = {
       --   macchiato = {
