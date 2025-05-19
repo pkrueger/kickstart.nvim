b4fb99e46ff052d4736c485f867fd3281a4494cf
Patrick Krueger <sendthistopatrick@gmail.com>
2025-05-19 01:25:18 -0600

    better lspconfig enable

--- lua/default/plugins/lspconfig.lua
+++ lua/default/plugins/lspconfig.lua
@@ -238,7 +238,11 @@ return {
         },
       },
     }
-
+    ---@type MasonLspconfigSettings
+    ---@diagnostic disable-next-line: missing-fields
+    require('mason-lspconfig').setup {
+      automatic_enable = vim.tbl_keys(servers or {}),
+    }
     -- Ensure the servers and tools above are installed
     --
     -- To check the current status of installed tools and/or manually install
@@ -258,20 +262,8 @@ return {
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-    require('mason-lspconfig').setup {
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_installation = true,
-      automatic_enable = true,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for ts_ls)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
+    for server_name, config in pairs(servers) do
+      vim.lsp.config(server_name, config)
+    end
   end,
 }
