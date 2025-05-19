6b8aee56219f66d00168c74626bf49f47d831a99
Patrick Krueger <sendthistopatrick@gmail.com>
2025-05-18 20:04:07 -0600

    update all default plugins

--- lua/default/plugins/todo-comments.lua
+++ lua/default/plugins/todo-comments.lua
@@ -2,5 +2,26 @@ return {
   'folke/todo-comments.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { signs = false },
+  keys = {
+    { '<leader>st', '<cmd>TodoTelescope<CR>', desc = '[S]earch [T]odo comments' },
+    { '<leader>ot', '<cmd>TodoTrouble<CR>', desc = 'Todo comments (Trouble)' },
+  },
+  opts = {
+    signs = true,
+    keywords = {
+      FIX = {
+        icon = ' ', -- icon used for the sign, and in search results
+        color = 'error', -- can be a hex color, or a named color (see below)
+        alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
+        -- signs = false, -- configure signs for some keywords individually
+      },
+      TODO = { icon = ' ', color = 'info' },
+      HACK = { icon = ' ', color = 'warning' },
+      WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+      PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+      NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+      TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+      REVIEW = { icon = ' ', color = 'info', alt = { 'REVIEW', 'REVIEWER' } },
+    },
+  },
 }
